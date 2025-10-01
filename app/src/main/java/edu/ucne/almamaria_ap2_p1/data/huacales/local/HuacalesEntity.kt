e4baf268fcdd20dc48df9943db98a0c6058cff48
Alma Maria <alma_maria@ucne.edu.do>
2025-10-01 19:29:50 -0400

    cree la entidad de huacales

---
+++ app/src/main/java/edu/ucne/almamaria_ap2_p1/data/huacales/local/HuacalesEntity.kt
@@ -0,0 +1,18 @@
+package edu.ucne.almamaria_ap2_p1.data.huacales.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.type.DateTime
+
+
+@Entity(tableName = "Huacales")
+class HuacalesEntity(
+    @PrimaryKey(autoGenerate = true)
+    val IdEntrada: Int = 0,
+    val Fecha: DateTime,
+    val NombreCliente: String = "",
+    val Cantidad: Int,
+    val Precio: Double
+)
+
+
