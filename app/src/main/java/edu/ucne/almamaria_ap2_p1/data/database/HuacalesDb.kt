bcce6f2bbb69431a19a7276a9ee783485d93d743
Alma Maria <alma_maria@ucne.edu.do>
2025-10-01 19:29:35 -0400

    base de datos

---
+++ app/src/main/java/edu/ucne/almamaria_ap2_p1/data/database/HuacalesDb.kt
@@ -0,0 +1,17 @@
+package edu.ucne.almamaria_ap2_p1.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import edu.ucne.almamaria_ap2_p1.data.huacales.local.HuacalesDao
+import edu.ucne.almamaria_ap2_p1.data.huacales.local.HuacalesEntity
+
+@Database(
+    entities = [
+        HuacalesEntity::class
+    ],
+    version = 1,
+    exportSchema = true
+)
+abstract class HuacalesDb: RoomDatabase(){
+    abstract fun HuacalesDb(): HuacalesEntity
+}
