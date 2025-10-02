5bc3120e0b81f452eb926207ba09fadf7e046ab6
Alma Maria <alma_maria@ucne.edu.do>
2025-10-01 23:31:50 -0400

    correccion de falta ortografica

--- app/src/main/java/edu/ucne/almamaria_ap2_p1/di/HuacalesRepositoryModule.kt
+++ app/src/main/java/edu/ucne/almamaria_ap2_p1/di/HuacalesRepositoryModule.kt
@@ -5,17 +5,17 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import edu.ucne.almamaria_ap2_p1.data.huacales.local.HuacalesEntity
+import edu.ucne.almamaria_ap2_p1.data.huacales.repository.HuacalesRepositoryImpl
 import edu.ucne.almamaria_ap2_p1.domain.huacales.repository.HuacalesRepository
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class HuacalesRepositoryModule {
-
     @Binds
     @Singleton
     abstract fun bindHucalaesRepository(
-        hucalesRepositoryImpl: HuacalesRepository
-    ): HuacalesEntity
+        hucalesRepositoryImpl: HuacalesRepositoryImpl
+    ): HuacalesRepository
 
 }
